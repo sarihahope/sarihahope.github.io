31baa123551a36bbc8a23ac3aa21d842bbdfc1b2
sarihahope <sarihaslink@yahoo.com>
2024-05-30 23:03:59 -0700

    Got the api to show up on the paghe

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
+const cards = document.querySelector('#cards');
 
-
-async function getBooks() {
+async function getApi () {
     const url = 'https://all-books-api.p.rapidapi.com/getBooks';
     const options = {
         method: 'GET',
@@ -9,30 +9,50 @@ async function getBooks() {
             'x-rapidapi-host': 'all-books-api.p.rapidapi.com'
         }
     };
-    
+
     try {
+
         const response = await fetch(url, options);
-        const result = await response.text();
+        const result = await response.json();
         console.log(result);
+        cards.innerHTML += `
+        <div class="card">
+            <img src="${result[0].bookImage}" alt="book image">
+            <h2>${result[0].bookTitle}</h2>
+            <p>${result[0].bookDescription}</p>
+            <p>Author: ${result[0].bookAuthor}</p>
+            <p>Publisher: ${result[0].bookPublisher}</p>
+            <a href="${result[0].amazonBookUrl}" target="_blank">Amazon</a>
+            <p>ISBN: ${result[0].bookIsbn}</p>
+            <p>Rank: ${result[0].bookRank}</p>
+        </div>
+        `;
     } catch (error) {
         console.error(error);
     }
 }
+getApi();
 
-getBooks();
-
-async function displayBooks() {
-    const booksContainer = document.querySelector('.books-container');
-    const books = await getBooks();
-    let template = '';
-    books.forEach(book => {
-        template += `
-            <div class="book">
-                <h2>${book.title}</h2>
-                <p>${book.author}</p>
-                <p>${book.year}</p>
-            </div>
-        `;
+function saveData() {
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const input = document.querySelector('#input').value;
+        localStorage.setItem('input', input);
     });
-    booksContainer.innerHTML = template;
 }
+saveData();
+
+function getData() {
+    const input = localStorage.getItem('input');
+    getApi(input);
+}
+getData();
+
+
+
+
+
+
+
+
